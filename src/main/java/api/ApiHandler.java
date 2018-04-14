1896999aad140223324f70c06df9a84ae756cfc6
Ian Anderson <iananderson@Ians-MacBook-Air.local>
2018-04-14 17:53:36 -0400

    began http request

--- src/main/java/api/ApiHandler.java
+++ src/main/java/api/ApiHandler.java
@@ -1,12 +1,51 @@
-//package api;
-//
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//
-//public static class ApiHandler {
-//    private final String USER_AGENT = "Mozilla/5.0";
-//
-//    URL url = new URL("http://example.com");
-//    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-//    con.setRequestMethod("GET");
-//}
+package api;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+
+public class ApiHandler {
+        private final String USER_AGENT = "Mozilla/5.0";
+
+            ApiHandler http = new ApiHandler();
+            http.sendGet();
+
+        // HTTP GET request
+        private void sendGet() throws Exception {
+
+            String url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/products/334\"";
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            // optional default is GET
+            con.setRequestMethod("GET");
+
+            //add request header
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            //print result
+            System.out.println(response.toString());
+
+        }
+
+    }
+
+}
