fb2e7405f2e2e214157d82206145586718bdc7de
Ian Anderson <iananderson@Ians-MacBook-Air.local>
2018-04-14 17:11:55 -0400

    adding files and updating JAR

---
+++ src/main/java/api/ApiHandler.java
@@ -0,0 +1,12 @@
+//package api;
+//
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//
+//public static class ApiHandler {
+//    private final String USER_AGENT = "Mozilla/5.0";
+//
+//    URL url = new URL("http://example.com");
+//    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//    con.setRequestMethod("GET");
+//}
