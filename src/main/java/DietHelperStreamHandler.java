a7725d251442d147f16630114d7102b2c13b2db1
GageMerrick <gtmerrick@plymouth.edu>
2018-04-14 17:08:11 -0400

    testing Intents

--- src/main/java/DietHelperStreamHandler.java
+++ src/main/java/DietHelperStreamHandler.java
@@ -3,19 +3,14 @@ import com.amazon.ask.Skill;
 import com.amazon.ask.SkillStreamHandler;
 
 import com.amazon.ask.Skills;
-import handlers.CancelandStopIntentHandler;
-import handlers.OneRestrictionHandler;
-import handlers.HelpIntentHandler;
-import handlers.SessionEndedRequestHandler;
-import handlers.LaunchRequestHandler;
-
+import handlers.*;
 
 
 public class DietHelperStreamHandler extends SkillStreamHandler {
 
     private static Skill getSkill() {
         return Skills.standard()
-                .addRequestHandlers(new CancelandStopIntentHandler(), new OneRestrictionHandler(), new HelpIntentHandler(), new LaunchRequestHandler(), new SessionEndedRequestHandler()).withSkillId("amzn1.ask.skill.51ecd59b-980d-441d-8162-67f511ad2481")
+                .addRequestHandlers(new CancelandStopIntentHandler(), new OneRestrictionHandler(), new HelpIntentHandler(), new LaunchRequestHandler(), new SessionEndedRequestHandler(), new TestIntentHandler()).withSkillId("amzn1.ask.skill.51ecd59b-980d-441d-8162-67f511ad2481")
                 .build();
     }
 
