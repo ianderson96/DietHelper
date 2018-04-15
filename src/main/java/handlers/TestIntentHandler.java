146dafffbdf13835da4cc650e2907bbbec08107d
mt1051 <mt1051@plymouth.edu>
2018-04-14 22:53:46 -0400

    changed connection

--- src/main/java/handlers/TestIntentHandler.java
+++ src/main/java/handlers/TestIntentHandler.java
@@ -31,9 +31,9 @@ public class TestIntentHandler implements RequestHandler {
           String databaseName = "users";
           String instanceConnectionName = "diethelper-201120:us-east1:users";
           //String jdbcUrl = "jdbc:mysql://google/%s?cloudSqlInstance=%s&socketFactory=com.google.cloud.sql.mysql.SocketFactory:"+instanceConnectionName+"/"+databaseName+"?user=root";
-          //Connection con = DriverManager.getConnection("jdbc:mysql://35.185.24.120:diethelper-201120:us-east1:users?user=root");
-          String jdbcUrl = String.format("jdbc:mysql://google/%s?cloudSqlInstance=%s&socketFactory=com.google.cloud.sql.mysql.SocketFactory"+databaseName+instanceConnectionName);
-          Connection con = DriverManager.getConnection(jdbcUrl,"root","DietHelper");
+          Connection con = DriverManager.getConnection("jdbc:mysql://35.185.24.120:diethelper-201120:us-east1:users?user=root");
+          //String jdbcUrl = String.format("jdbc:mysql://google/%s?cloudSqlInstance=%s&socketFactory=com.google.cloud.sql.mysql.SocketFactory"+databaseName+instanceConnectionName);
+          Connection con = DriverManager.getConnection(jdbcUrl,"root");
           //speechText = "Hello world: you are in the test intent";
         }
           catch ( SQLException err ) {
@@ -42,7 +42,7 @@ public class TestIntentHandler implements RequestHandler {
         
 
         return input.getResponseBuilder()
-                .withSpeech("speechText")
+                .withSpeech(speechText)
                 .build();
     }
 
